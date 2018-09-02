df56cf49245910e794b936939b84a67e457b125b
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-02 17:05:16 -0400

    Test sliding window function

--- tests/test_common.py
+++ tests/test_common.py
@@ -11,3 +11,14 @@ class TestCommonMethods(unittest.TestCase):
         image = cutoff_lower(data.astronaut(), percent)
         self.assertEqual(image.shape[0], int(512 - (512 * percent)))
 
+    def test_sliding_window(self):
+        image = data.astronaut()
+        window_size = (32, 64)
+        step_size = (16, 16)
+
+        # Check that the window size is correct every time
+        for i, (x, y, window) in enumerate(sliding_window(image, window_size, step_size)):
+            self.assertEqual((64, 32), window.shape[:2])
+
+        total_windows = int((512-16) / step_size[0]) * int((512-48) / step_size[1])
+        self.assertEqual(i+1, total_windows)
