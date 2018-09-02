1bc68656fc0d60bbf79f430cd80d8fb4c0e97658
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-02 17:05:35 -0400

    Sliding window function

--- src/common.py
+++ src/common.py
@@ -1,4 +1,13 @@
 def cutoff_lower(image, percent):
+    """ Remove the lower part of an image """
     y = image.shape[0]
     cutoff = int(y - (percent * y))
     return image[:cutoff, :]
+
+
+def sliding_window(image, window_size, step_size):
+    """ Run a sliding window across an image and extract each window """
+    for y in range(0, image.shape[0] - (window_size[1] - step_size[1]), step_size[1]):
+        for x in range(0,  image.shape[1] - (window_size[0] - step_size[0]), step_size[0]):
+            window = image[y: y+window_size[1], x:x + window_size[0]]
+            yield x, y, window
