fd80eac1a5a932ac2d679aebefb0d636fb3e007c
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-02 20:05:34 -0400

    Initial commit

---
+++ src/spotlight_detector.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+""" Identify the regions of interest in an image by
+        1. Locating spotlights (bright spots) through top-hat morphology
+        2. Perform a region growing algorithm (watershed) with the spotlights as the seeds 
+        3. Selecting the spotlights that do not grow too large """
+
+
+class SpotlightDetector:
+    def __init__(self, threshold, max_size, kernel):
+        self.threshold = threshold
+        self.max_size = max_size
+        self.kernel = kernel
+
+    def compute_roi(self, image):
+        """ Find the spotlights and perform region growing. Store markers that do not grow too large """
+
+        # Perform top-hat morphology to find the spotlights in the image
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        tophat_image = cv2.morphologyEx(gray_image, cv2.MORPH_TOPHAT, self.kernel)
+        ret, thresh = cv2.threshold(tophat_image, self.threshold, 255, cv2.THRESH_BINARY)
+
+        # Watershed region growing algorithm with the spotlights as the seeds
+        dist_transform = cv2.distanceTransform(thresh, cv2.DIST_L2, 5)
+        ret, markers = cv2.connectedComponents(np.uint8(dist_transform))
+        watershed_image = cv2.watershed(image, markers)
+
+        # Grab the marker values and how many times they occur
+        values, counts = np.unique(watershed_image, return_counts=True)
+
+        # Get the indices of where the segments are under the max size
+        segment_indices = np.where(counts <= self.max_size)
+        markers = values[segment_indices]
+
+        # Get the median coordinates of the markers (roughly the center)
+        for marker in markers:
+            y_coordinates, x_coordinates = np.where(watershed_image == marker)
+            yield int(np.median(x_coordinates)), int(np.median(y_coordinates))
+
+    def display_roi(self, image, window_size):
+        """ Construct windows around the center of the identified ROI and display the image """
+        mask = np.zeros(image.shape, dtype=np.uint8)
+        x_offset = int((window_size[0] - 1) / 2)
+        y_offset = int((window_size[1] - 1) / 2)
+
+        for (x, y) in self.compute_roi(image):
+            x_min, x_max = x - x_offset, x + x_offset
+            y_min, y_max = y - y_offset, y + y_offset
+            mask[y_min:y_max, x_min:x_max] = 1
+
+        display_img = np.zeros(image.shape, dtype=np.uint8)
+        display_img[mask == 1] = image[mask == 1]
+
+        plt.imshow(display_img, cmap='gray')
+        plt.show()
