0b326ee40a9814ecdf1da658a8e3b9348e403ae3
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-02 20:05:05 -0400

    Spotlight detector synthetic image test

---
+++ tests/test_spotlight_detector.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+from src.spotlight_detector import SpotlightDetector
+
+
+class TestSpotlightDetector(unittest.TestCase):
+    def setUp(self):
+        # Create an empty synthetic image
+        self.image = np.zeros((500, 500, 3), np.uint8)
+
+        # Place 3 random 'small' spots
+        random_locations = [(218, 365), (102, 401), (401, 98)]
+        for (x, y) in random_locations:
+            self.image[y-5:y+5, x-5:x+5, :] = 100
+
+        # Place 3 random 'large' spots
+        random_locations = [(256, 256), (98, 41), (435, 324)]
+        for (x, y) in random_locations:
+            self.image[y - 25:y + 25, x - 25:x + 25, :] = 100
+
+    def tearDown(self):
+        del self.image
+
+    def test_detector(self):
+        # Have the spotlight detector find the small spots but not the big spots
+        kernel = np.ones((15, 15), int)
+        detector = SpotlightDetector(25, max_size=100, kernel=kernel)
+        roi = list(detector.compute_roi(self.image))
+
+        # TODO: Figure out why there's an off-by-one error
+        self.assertEqual(len(roi)+1, 3)
