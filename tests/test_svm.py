365644a8f63fe05cc4fe60df90afd7676baf8086
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-08 17:09:45 -0400

    Set up common variables

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -6,17 +6,19 @@ from src.svm import *
 
 class TestSVM(unittest.TestCase):
 
+    def setUp(self):
+        self.descriptor = HogDescriptor(win_size=(64, 128), block_size=(16, 16), block_stride=(8, 8), cell_size=(8, 8),
+                                        orientations=9)
+        self.clf = SVM(self.descriptor)
+
+        self.images = [data.astronaut(), data.hubble_deep_field(), data.coffee()]
+        self.images = [cv2.resize(im, (256, 256)) for im in self.images]
+
     def test_svm_train(self):
         """ Make sure the train function can run """
 
-        descriptor = HogDescriptor(win_size=(64, 128), block_size=(16, 16), block_stride=(8, 8), cell_size=(8, 8),
-                                   orientations=9)
-
-        clf = SVM(descriptor)
-
-        images = [data.astronaut(), data.hubble_deep_field(), data.coffee()]
-        images = [cv2.resize(im, (256, 256)) for im in images]
-        labels = [0, 0, 0]
-
-        res = clf.train(images, labels)
+        res = self.clf.train(self.images, labels=[0, 0, 0])
         self.assertIsNotNone(res)
+
+    def test_svm_predict(self):
+        pass
