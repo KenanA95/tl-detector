dcb0dfdaaa792bd712687f845ae355a79ee136be
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-08 17:26:20 -0400

    Make initial tests run

--- src/svm.py
+++ src/svm.py
@@ -1,13 +1,25 @@
 import cv2
+from src.features import Descriptor
+from sklearn import linear_model
 
 
 class SVM:
-    def __init__(self):
-        pass
+    def __init__(self, descriptor):
+        """
+        Linear SVM trained with stochastic gradient descent (SGD)
+        :param descriptor: Feature Descriptor Object that converts images into vectors
+        """
+        self.descriptor = descriptor
+        self.clf = linear_model.SGDClassifier()
 
-    def train(self):
-        pass
+    def train(self, images, labels):
+        features = [self.descriptor.compute(im) for im in images]
+        self.clf.fit(features, labels)
+        return self.clf
 
-    def predict(self):
-        pass
+    def predict(self, image):
+        fd = self.descriptor.compute(image).reshape(1, -1)
+        return self.clf.predict(fd)[0]
 
+    def predict_all(self, images):
+        return [self.predict(image) for image in images]
