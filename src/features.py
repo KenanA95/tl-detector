b74cf63fa2eaf93d73721d509a9974977df3e2fe
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-08 16:40:15 -0400

    Create base Descriptor class

--- src/features.py
+++ src/features.py
@@ -2,13 +2,28 @@ from skimage.feature import hog
 from matplotlib import pyplot as plt
 
 
-class HogDescriptor:
+class Descriptor:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def compute(self, **kwargs):
+        pass
+
+    def display(self, **kwargs):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class HogDescriptor(Descriptor):
     def __init__(self, win_size, block_size, block_stride, cell_size, orientations):
         self.win_size = win_size
         self.block_size = block_size
         self.block_stride = block_stride
         self.cell_size = cell_size
         self.orientations = orientations
+        Descriptor.__init__(self)
 
     def compute(self, image, multichannel=True, visualize=False):
         return hog(image, self.orientations, pixels_per_cell=self.cell_size, cells_per_block=self.block_size,
@@ -24,14 +39,18 @@ class HogDescriptor:
                .format(self.win_size, self.block_size, self.block_stride, self.cell_size, self.orientations)
 
 
-class LBPDescriptor:
+class LBPDescriptor(Descriptor):
     def __init__(self, points, radius, method='default'):
         self.points = points
         self.radius = radius
         self.method = method
+        Descriptor.__init__(self)
 
     def compute(self):
         pass
 
     def display(self):
+        pass
+
+    def __repr__(self):
         pass
