d8b03f47defe7214134f20b6e14d471b6982107b
kenana95 <kenan.r.alkiek@gmail.com>
2018-09-08 17:48:46 -0400

    Cross-train evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
 
 
 def evaluate_model(classifier, images, labels):
@@ -13,3 +14,8 @@ def evaluate_model(classifier, images, labels):
     print("Precision: {:.2f}".format(precision))
 
 
+def cross_train_validate(classifier, images, labels, test_size):
+    """ Split a data-set and train a classifier. See how well it performs """
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=test_size)
+    classifier.train(X_train, y_train)
+    evaluate_model(classifier, images, labels)
