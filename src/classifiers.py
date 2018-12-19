fa8bbc6d6dd9eb049445f84e5a8099da8fd52593
Kenan Alkiek <kenan.r.alkiek@gmail.com>
2018-12-18 20:18:35 -0500

    Switch to SVC

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,23 +1,29 @@
-from sklearn import linear_model
+from sklearn.svm import SVC
+from sklearn import preprocessing
 from src.helpers import sliding_window, extract_window
 
 
 class SVM:
-    def __init__(self, descriptor):
+    def __init__(self, descriptor, C=1.0):
         """
-        Linear SVM trained with stochastic gradient descent (SGD)
+        C-Support Vector Classification
         :param descriptor: Feature Descriptor Object that converts images into vectors
+        :param C: Penalty parameter C of the error term
         """
-        self.clf = linear_model.SGDClassifier()
+        self.C = C
+        self.clf = SVC(C)
         self.descriptor = descriptor
+        self.scaler = preprocessing.StandardScaler()
 
     def train(self, images, labels):
-        features = [self.descriptor.compute(im) for im in images]
+        features = [self.descriptor.compute(img) for img in images]
+        features = self.scaler.fit(features).transform(features)
         self.clf.fit(features, labels)
         return self.clf
 
     def predict(self, image):
         fd = self.descriptor.compute(image).reshape(1, -1)
+        fd = self.scaler.transform(fd)
         return self.clf.predict(fd)[0]
 
     def predict_all(self, images):
@@ -33,6 +39,9 @@ class SVM:
             window = extract_window(image, (x, y), win_size)
             if window is not None and self.predict(window):
                 yield (x, y)
+
+    def __repr__(self):
+        return "SVM with C Penalty: {}".format(self.C)
 
 
 class Cascade:
