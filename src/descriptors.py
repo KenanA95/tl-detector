504442adc6f3836e5a8c617fbd363c60b513588e
Kenan Alkiek <kenan.r.alkiek@gmail.com>
2018-12-29 15:55:34 -0500

    Start move of haar feature extraction into descriptors

--- src/descriptors.py
+++ src/descriptors.py
@@ -1,7 +1,12 @@
 import numpy as np
-from skimage.feature import hog, local_binary_pattern
-from matplotlib import pyplot as plt
 from ast import literal_eval
+from matplotlib import pyplot as plt
+from dask import delayed
+from skimage.feature import hog, local_binary_pattern
+from skimage.transform import integral_image
+from skimage.feature import haar_like_feature, haar_like_feature_coord, draw_haar_like_feature
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 class HogDescriptor:
@@ -61,18 +66,60 @@ class LBPDescriptor:
 
 
 class HaarDescriptor:
-    def __init__(self):
-        pass
+    def __init__(self, feature_types, selected_feature_type=None, selected_feature_coord=None):
+        self.feature_types = feature_types
+        self.selected_feature_type = selected_feature_type
+        self.selected_feature_coord = selected_feature_coord
 
-    def compute(self):
-        pass
+    def compute(self, image):
+        ii = integral_image(image)
+        return haar_like_feature(ii, 0, 0, ii.shape[0], ii.shape[1],
+                                 self.selected_feature_type, self.selected_feature_coord)
 
-    def display(self):
-        pass
+    def display(self, significant_features, image):
+        """ Plot the most significant haar-like features on top of an image """
+        feature_coord, _ = haar_like_feature_coord(width=32, height=64, feature_type=self.feature_types)
+
+        fig, axes = plt.subplots(3, 2)
+        for idx, ax in enumerate(axes.ravel()):
+            image = draw_haar_like_feature(image, 0, 0, 32, 64, [feature_coord[significant_features[idx]]])
+            ax.imshow(image)
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        fig.suptitle('The most important features')
+        plt.show()
+
+    def get_selected_features(self, images, labels, size=(32, 64)):
+        """ """
+        # Compute the haar-like features for all of the different feature types
+        X = delayed(self.compute(img) for img in images)
+        X = np.array(X.compute(scheduler='processes'))
+        X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.25)
+
+        # Train a random forest classifier to find out which features are important for training
+        clf = RandomForestClassifier(n_estimators=1000, max_depth=None, max_features=100, n_jobs=-1, random_state=0)
+        clf.fit(X_train, y_train)
+        significant_features = np.argsort(clf.feature_importances_)[::-1]
+
+        cdf_feature_importances = np.cumsum(clf.feature_importances_[significant_features])
+        cdf_feature_importances /= np.max(cdf_feature_importances)
+        sig_feature_count = np.count_nonzero(cdf_feature_importances < 0.7)
+        sig_feature_percent = round(sig_feature_count / len(cdf_feature_importances) * 100, 1)
+
+        print(('{} features, or {}%, account for {}% of branch points in the random '
+               'forest.').format(sig_feature_count, sig_feature_percent, 0.7))
+
+        # Extract all possible features to be able to select the most salient
+        feature_coord, feature_type = haar_like_feature_coord(width=size[0], height=size[1],
+                                                              feature_type=self.feature_types)
+        # Store the most informative features
+        self.selected_feature_coord = feature_coord[significant_features[:sig_feature_count]]
+        self.selected_feature_type = feature_type[significant_features[:sig_feature_count]]
 
     @classmethod
     def from_config_file(cls, settings):
-        return cls()
+        pass
 
     def __repr__(self):
         pass
